95aa29037ecd6f4ed116f54ca2a28d447a2f2f69
naveenvignesh <naveenvignesh5.nv@gmail.com>
2017-12-20 14:30:51 +0530

    added twitter function

--- functions.R
+++ functions.R
@@ -1,1 +1,13 @@
 # R script containing all mathematical functions
+
+setupTwitterAPI <- function() {
+    setup_twitter_oauth(apiKey,apiSecret,access_token,access_token_secret)
+}
+
+setupDatabase <- function() {
+    register_mysql_backend(dbname,dbhost,dbuser,dbpass)
+}
+
+filterData <- function(terms) {
+    
+}
