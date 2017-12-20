40193a44eedbf1a8ffe391eb7718afc6d2f0d650
naveenvignesh <naveenvignesh5.nv@gmail.com>
2017-12-20 09:41:58 +0530

    initialized the environment setup

---
+++ environment.R
@@ -0,0 +1,8 @@
+# Twitter API keys
+apiKey <- 'yAmS3louyR8diGxBwzJtLfgdE'
+apiSecret <- 'GY40lRt62X7s8VxcFFeFOsgnKbqYHtJsuh6KAhE4JfnVntoKT5'
+access_token <- '2195494530-V7csBiOqiffi69fZuhkZEqMRZO5xqHHWcSiBl9V'
+access_token_secret <- 'CQ96GaCgeIFqq2XVPtigJnFVD9momtZqFBEsHpH8n7q3Z'
+
+# download.file(url="https://curl.haxx.se/ca/cacert.pem",destfile="cacert.pem")
+
---
+++ packages.R
@@ -0,0 +1,6 @@
+# libraries to be needed
+library(twitteR)
+library(ROAuth)
+library(RColorBrewer)
+library(ggplot2)
+library(base64enc)
