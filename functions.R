e511705cda613d0be6dbe7eb8c8cc2926e33e0a1
naveenvignesh <naveenvignesh5.nv@gmail.com>
2018-01-01 20:51:25 +0530

    added sentiment analysis functions

--- functions.R
+++ functions.R
@@ -50,12 +50,14 @@ AddItemNaive <- function(item,arr) {
   arr[[length(.GlobalEnv$Result)+1]] <- item
 }
 
-queryTweets <- function(query,corpus) {
+queryTweets <- function(query,corpus,org_tweets_df) {
+    tweets.df <- twListToDF(org_tweets_df)
+    myCorpus <- Corpus(VectorSource(tweets.df$text)) 
     res = c()
     for(i in 1:length(corpus)) {
         temp <- unlist(strsplit(toString(corpus[[i]]$content),split=" "))
         if(query %in% temp) {
-            res <- c(res,corpus[[i]]$content)
+            res <- c(res,myCorpus[[i]]$content)
         }
     }
     res <- unique(res)
@@ -65,6 +67,77 @@ queryTweets <- function(query,corpus) {
 # ----------------------- REMOVING DUPLICATES ---------------------------
 
 # method to remove near duplicates
-shingleVector <- function(vector) {
+# shingleVector <- function(vector) {
     
+# }
+
+#------------------------- SENTIMENT ANALYSIS -------------------------------
+
+setupPosNeg <- function(posText,negText) {
+    posText <- read.delim("positive.txt", header=FALSE, stringsAsFactors=FALSE)
+    posText <- posText$V1
+    posText <- unlist(lapply(posText, function(x) { str_split(x, "\n") }))
+    negText <- read.delim("negative.txt", header=FALSE, stringsAsFactors=FALSE)
+    negText <- negText$V1
+    negText <- unlist(lapply(negText, function(x) { str_split(x, "\n") }))
+    pos.words = c(posText, 'upgrade')
+    neg.words = c(negText, 'wtf', 'wait', 'waiting','epicfail', 'mechanical')
+    return(list("pos"=pos.words,"neg"=neg.words))
+}
+
+score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
+{
+# Parameters
+# sentences: vector of text to score
+# pos.words: vector of words of positive sentiment
+# neg.words: vector of words of negative sentiment
+# .progress: passed to laply() to control of progress bar
+# create a simple array of scores with laply
+    scores = laply(sentences, function(sentence, pos.words, neg.words) {
+        # remove punctuation
+        sentence = gsub("[[:punct:]]", "", sentence)
+        # remove control characters
+        sentence = gsub("[[:cntrl:]]", "", sentence)
+        # remove digits?
+        sentence = gsub('\\d+', '', sentence)
+        # define error handling function when trying tolower
+        tryTolower = function(x)
+        {
+            # create missing value
+            y = NA
+            # tryCatch error
+            try_error = tryCatch(tolower(x), error=function(e) e)
+            # if not an error
+            if (!inherits(try_error, "error"))
+            y = tolower(x)
+            # result
+            return(y)
+        }
+        # use tryTolower with sapply 
+        sentence = sapply(sentence, tryTolower)
+        # split sentence into words with str_split (stringr package)
+        word.list = str_split(sentence, "\\s+")
+        words = unlist(word.list)
+        # compare words to the dictionaries of positive & negative terms
+        pos.matches = match(words, pos.words)
+        neg.matches = match(words, neg.words)
+        # get the position of the matched term or NA
+        # we just want a TRUE/FALSE
+        pos.matches = !is.na(pos.matches)
+        neg.matches = !is.na(neg.matches)
+        # final score
+        score = sum(pos.matches) - sum(neg.matches)
+        return(score)
+        }, pos.words, neg.words, .progress=.progress )
+    
+    # data frame with scores for each sentence
+    scores.df = data.frame(text=sentences, score=scores)
+    return(scores.df)
+}
+
+calculateSentiment <- function(tweets) {
+    sample <- setupPosNeg()
+    score = score.sentiment(tweets,sample$pos,sample$neg,.progress="text")
+    print(score)
+    return(score)
 }
